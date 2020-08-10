17fd4e23393cdb3765e4e85d32cc365fcb4512eb
Shady Khalifa <shekohex@gmail.com>
2020-08-10 17:45:58 +0200

    add CI/CD

---
+++ ci/before_deploy.sh
@@ -0,0 +1,31 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    # TODO Update this to build the artifacts that matter to you
+    cargo build --bin impulse --target $TARGET --release
+
+    # TODO Update this to package the right artifacts
+    cp target/$TARGET/release/impulse $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
